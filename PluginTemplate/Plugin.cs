4cf01a75685141744e527e1184288bb11d54953a
hiroki199990625 <hiroki19990625@gmail.com>
2018-06-23 20:07:17 +0900

    Fix...

---
+++ PluginTemplate/Plugin.cs
@@ -0,0 +1,26 @@
+﻿using MineNET;
+using MineNET.Plugins;
+
+namespace PluginTemplate
+{
+    public class Plugin : PluginBase
+    {
+        public override string Name
+        {
+            get
+            {
+                return "PluginTemplate";
+            }
+        }
+
+        public override void OnEnable()
+        {
+            OutLog.Info($"> Enable {this.Name}!!");
+        }
+
+        public override void OnDisable()
+        {
+            OutLog.Info($"> Disable {this.Name}!!");
+        }
+    }
+}
