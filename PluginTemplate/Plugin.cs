e5e5f2c59482fb0b5149d03c3bf5dc3b827949df
hiroki19990625 <hiroki19990625@gmail.com>
2019-06-02 01:59:15 +0900

    パッケージを更新

--- PluginTemplate/Plugin.cs
+++ PluginTemplate/Plugin.cs
@@ -1,26 +1,21 @@
 ﻿using MineNET;
+using MineNET.IO;
 using MineNET.Plugins;
 
 namespace PluginTemplate
 {
     public class Plugin : PluginBase
     {
-        public override string Name
-        {
-            get
-            {
-                return "PluginTemplate";
-            }
-        }
+        public override string Name => "PluginTemplate";
 
         public override void OnEnable()
         {
-            OutLog.Info($"> Enable {this.Name}!!");
+            Logger.Info($"> Enable {this.Name}!!");
         }
 
         public override void OnDisable()
         {
-            OutLog.Info($"> Disable {this.Name}!!");
+            Logger.Info($"> Disable {this.Name}!!");
         }
     }
 }
